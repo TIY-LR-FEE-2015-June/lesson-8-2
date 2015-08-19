e9fdd356218b4b01f4344b09b307445e8f9421ef
Ryan Tablada <ryan.tablada@gmail.com>
2015-08-19 12:38:55 -0500

    Update edit form for contact

--- app/contacts/edit/route.js
+++ app/contacts/edit/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  /**
+   * This code is automatically run because the
+   * route parameter was called `contact_id`
+   */
+  // model: function(params) {
+  //   return this.store.findById('contact', params.contact_id);
+  // }
+
+  actions: {
+    save: function(contact, data) {
+      contact.setProperties(data);
+
+      contact.save().then(() => {
+        this.transitionTo('contacts.show', contact);
+      });
+    }
+  }
 });
